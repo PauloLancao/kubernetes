96169a1b862fad65c46d249d7e1e78ee3a8d8946
PauloLancao <paulolancao@hotmail.com>
2022-01-25 13:51:58 +0000

    Add liquibase-init project helm charts

--- k8-liquibase-init/build.sh
+++ k8-liquibase-init/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sed -i 's/^[#]*\s*appVersion:.*/appVersion: "'$1'"/g' 'src/main/helm/application/Chart.yaml'
 mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1
 mvn clean deploy
